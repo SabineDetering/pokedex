869d321d40e5d8a1a4a7e4a6813c6f118d3074f8
Sabine Detering <sabine.detering@web.de>
2022-02-23 19:22:25 +0100

    loading and first rendering of small cards

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+let pokeInfos = [];
 let currentPokemon;
 
-async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/charmander`;
-    let response = await fetch(url);
-    console.log(response);
-    currentPokemon = await response.json();
-    console.log(currentPokemon);
-    renderPokemon();
+function getId(id) {
+    return document.getElementById(id);
 }
+function capitalizeFirstLetter(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+
+// loading parallel?
+function load(maxNumber) {
+    for (let i = 1; i <= maxNumber; i++) {
+        loadPokemon(i);
+    }
+}
+
+async function loadPokemon(id) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let response = await fetch(url);
+    pokemon = await response.json();
+    pokeInfos.push(pokemon);
+    renderCard(id);
+}
+
+function renderCard(i) {
+    let cards = getId('small-cards');
+
+    const pokemon = pokeInfos[i];
+    let name = pokemon.name;
+    console.log(name);
+    let types = pokemon.types;
+    let id = pokemon.id;
+    let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`;
+
+    cards.innerHTML += ` 
+            <div id='card-${i}' class="small-pokecard">
+                <h2>${capitalizeFirstLetter(name)}</h2>
+                 <img src="${image}" >
+            </div>
+        `;
+
+}
+
+
 function renderPokemon() {
     let container = document.getElementById('pokemon-container');
     container.innerHTML = `
