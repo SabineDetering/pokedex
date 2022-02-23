4c8e3396aa55572dda37f4616ea68e3b59d99ded
Sabine Detering <sabine.detering@web.de>
2022-02-24 00:11:36 +0100

    small cards done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currentPokemon;
 function getId(id) {
     return document.getElementById(id);
 }
-function capitalizeFirstLetter(string) {
+function capFirstLetter(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
 
@@ -24,30 +24,44 @@ async function loadPokemon(id) {
     renderCard(id);
 }
 
-function renderCard(i) {
+function renderCard(id) {
     let cards = getId('small-cards');
+    let index = id - 1;//Index in array 1 lower than id
 
-    const pokemon = pokeInfos[i];
+    const pokemon = pokeInfos[index];
     let name = pokemon.name;
-    console.log(name);
+    let pokeId = pokemon.id;
+    let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokeId}.png`;
+
     let types = pokemon.types;
-    let id = pokemon.id;
-    let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`;
-
+    let mainType = types[0].type.name;
+    let secondType = '';
+    if (types.length == 2) {
+        secondType = types[1].type.name;
+    }
+    let typeDivs = `
+        <div class="type px-3 py-2">${capFirstLetter(mainType)}</div>`;
+    if (secondType) {
+        typeDivs += `
+        <div id="type2-${pokeId}" class="type px-3 py-2">${capFirstLetter(secondType)}</div>`;
+    }
     cards.innerHTML += ` 
-            <div id='card-${i}' class="small-pokecard">
-                <h2>${capitalizeFirstLetter(name)}</h2>
-                 <img src="${image}" >
+    <div id='card-${pokeId}' class="small-pokecard m-2 p-4">
+        <div  class="d-flex justify-content-between"> 
+            <h2>${capFirstLetter(name)}</h2>
+            <span class="">#${pokeId}</span>
+        </div>
+        <div class="d-flex justify-content-between"> 
+            <div>                       
+                 ${typeDivs}       
             </div>
+            <img src="${image}" >
+        </div>
+     </div>
         `;
-
+    getId('card-' + pokeId).style.backgroundColor = `var(--bg-${mainType})`;
+    if (secondType) {
+        getId('type2-' + pokeId).style.backgroundColor = `var(--bg-${secondType})`;
+    }
 }
 
-
-function renderPokemon() {
-    let container = document.getElementById('pokemon-container');
-    container.innerHTML = `
-        <h1>${currentPokemon.name}</h1>
-        <img src=${currentPokemon.sprites.front_shiny} alt="">
-    `;
-}
