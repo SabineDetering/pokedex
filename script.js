991db43e30176e2a9635bd605e597f98a210c7fc
Sabine Detering <sabine.detering@web.de>
2022-02-25 01:51:06 +0100

    big card upper part mainly finished, tabs in lower part not yet working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let pokeInfos = [];
 let pokeExtract = [];
-let currentPokemon;
+let currPokemon;
 
 function getId(id) {
     return document.getElementById(id);
@@ -11,9 +11,9 @@ function capFirstLetter(string) {
 
 
 // loading parallel?
-function load(maxNumber) {
+async function load(maxNumber) {
     for (let i = 1; i <= maxNumber; i++) {
-        loadPokemon(i);
+        await loadPokemon(i);
     }
 }
 
@@ -48,12 +48,11 @@ function renderSmallCard(id) {
     // let pokemon = getPokeInfos(id);
     let pokemon = pokeExtract[id];
 
-    // let typeDivs = renderTypeDivs(pokemon.mainType, pokemon.secondType);
     cards.innerHTML += ` 
-    <div id='card-${pokemon.pokeId}' class="small-pokecard container m-2 p-4" onclick="showBigCard(${pokemon.pokeId})">
+    <div id='card-${pokemon.pokeId}' class="small-card container m-2 p-4" onclick="showBigCard(${pokemon.pokeId})">
         <div  class="d-flex justify-content-between"> 
             <h2>${capFirstLetter(pokemon.name)}</h2>
-            <span class="">#${pokemon.pokeId}</span>
+            <h4 >#${pokemon.pokeId}</h4>
         </div>
         <div class="d-flex justify-content-between"> 
             <div>                       
@@ -68,7 +67,7 @@ function renderSmallCard(id) {
 
 function renderTypeDivs(main, second) {
     let htmlcode =
-        `<div class="type mt-1 mr-1 px-3 py-2">${capFirstLetter(main)}</div>`;
+        `<div class="type mt-1 me-1 px-3 py-2">${capFirstLetter(main)}</div>`;
     if (second) {
         htmlcode += `
         <div class="type mt-1 px-3 py-2">${capFirstLetter(second)}</div>`;
@@ -88,46 +87,60 @@ function colorizeCard(cardid, main, second) {
 function showBigCard(id) {
     getId('blur').classList.remove('d-none');
     document.body.style = 'overflow:hidden;';
-    getId('big-card').classList.remove('d-none');
+    // getId('big-card-upper').classList.remove('d-none');
+    // getId('big-card-lower').classList.remove('d-none');
     renderBigCard(id);
 }
-function closeBigCard(id) {
+function closeBigCard() {
     getId('blur').classList.add('d-none');
     document.body.style = 'overflow:auto;';
-    getId('big-card').classList.add('d-none');
+    // getId('big-card-upper').classList.add('d-none');
+    // getId('big-card-lower').classList.add('d-none');
 }
 
 function renderBigCard(id) {
-    renderNav();
-    renderHeader(id);
-    renderInfos(id);
+    currPokemon = pokeExtract[id];
+    renderNav(id);
+    renderHeader();
+    renderInfos();
 }
-function renderNav() {
+function renderNav(id) {
+    if (id > 1) {
+        getId('arrow-left').onclick = function () { renderBigCard(id - 1); };
+    } else {
+        getId('arrow-left').onclick = function () { closeBigCard(); }
+    }
 
+    if (id < pokeExtract.length-1) {
+        getId('arrow-right').onclick = function () { renderBigCard(id + 1); };
+    } else {
+        getId('arrow-right').onclick = async function () { await loadPokemon(id + 1);  renderBigCard(id + 1); }
+    }
 }
-function renderHeader(id) {
+function renderHeader() {
     let card = getId('header');
-    let pokemon = pokeExtract[id];
 
-    card.innerHTML += `     
+    card.innerHTML = `     
         <div  class="d-flex justify-content-between"> 
             <div>
-                <h2>${capFirstLetter(pokemon.name)}</h2>
+                <h2>${capFirstLetter(currPokemon.name)}</h2>
                 <div class="type-wrapper">
-                     ${renderTypeDivs(pokemon.mainType, pokemon.secondType)}
+                     ${renderTypeDivs(currPokemon.mainType, currPokemon.secondType)}
                 </div>
             </div>
-            <div class="d-flex flex-column align-items-end"> 
-                <img src="./img/heart-outline.png">
-                <span class="">#${pokemon.pokeId}</span>        
+            <div class="d-flex flex-column align-items-end mt-1"> 
+                <h4 class="">#${currPokemon.pokeId}</h4>    
+                <img id="heart" src="./img/heart-outline.png">    
             </div>
         </div>
-            <img src="${pokemon.image}" >
+        <div class="d-flex justify-content-center"><img class="big-pic" src="${currPokemon.image}"></div>
      
     `;
-    colorizeCard('big-card', pokemon.mainType, pokemon.secondType);
+    colorizeCard('big-card-upper', currPokemon.mainType, currPokemon.secondType);
 }
 
 
-function renderInfos(id) { }
+function renderInfos() {
+    
+}
 
